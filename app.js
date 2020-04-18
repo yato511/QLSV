bf8893184c1682b7b7577ccd2936bb6651d11444
Tran Quoc Toan <tranquoctoan51199@gmail.com>
2020-04-18 23:00:39 +0700

    prepare

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use("/student", require("./routes/student.routes"));
 app.use("/faculty", require("./routes/faculty.routes"));
 app.use("/", (req, res) => res.redirect("/student/1"));
 
-app.listen(3000, () => {
-	console.log(`Server is running on port 3000`);
+let port = process.env.PORT;
+if (port == null || port == "") {
+	port = 8000;
+}
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
 });
